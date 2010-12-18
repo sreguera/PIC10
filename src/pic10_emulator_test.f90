1314b2786207e30841caf97dc55f4c099cdd6f84
Sebastián <sreguera@gmail.com>
2010-12-18 23:09:05 +0100

    Added test program for the emulator.

---
+++ src/pic10_emulator_test.f90
@@ -0,0 +1,14 @@
+!------------------------------------------------------------------------
+! Copyright (c) 2010 Jose Sebastian Reguera Candal.
+! This file is part of PIC10, The PIC10 microcontroller emulator.
+!------------------------------------------------------------------------
+
+! Test program for the PIC10 emulator
+!
+program pic10_emulator_test
+  use pic10_emulator
+  implicit none
+
+contains
+
+end program pic10_emulator_test
